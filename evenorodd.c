fc2c7d25f7d89e5d57d89c513e60c3354e3c17d7
aksprogramming <47626967+aksprogramming@users.noreply.github.com>
2019-03-01 13:44:50 +0530

    Add files via upload

---
+++ evenorodd.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+
+	int a;
+
+	printf("Enter the Number \n");
+	scanf("%d",&a);
+
+	if(a%2==0)
+
+		printf("The Given number is even\n");
+
+
+	else
+
+		printf("The given number is odd\n");
+
+	
+
+	return 0;
+}
